f958b75289b6352fa34ad0329d1c7160368cd0d0
Marcela Oliveira <magaeu@gmail.com>
2013-06-28 00:50:09 -0300

    added i18n pt-BR version

---
+++ assets/javascripts/i18n/pt-BR.js
@@ -0,0 +1,10 @@
+$.timerlog_timer_i18n = {
+	minutes: ['{0} minuto', '{0} minutos'],
+	hours: ['{0} horas', '{0} horas'],
+	title: 'Contador',
+	activeTimer: 'Há um contador funcionando.',
+	actionMerge: 'Adicionar com a atual',
+	remove: 'Parar e remover',
+	openForm: 'Abrir formulário',
+	removeConfirm: 'Tem certeza?'
+};
