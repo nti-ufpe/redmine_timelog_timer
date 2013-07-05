38d33da9aaf4de53f5efef992aa46a95307df064
Marcela Oliveira <magaeu@gmail.com>
2013-07-04 21:38:40 -0300

    fixed encoding problems

--- assets/javascripts/i18n/pt-BR.js
+++ assets/javascripts/i18n/pt-BR.js
@@ -2,9 +2,9 @@ $.timerlog_timer_i18n = {
 	minutes: ['{0} minuto', '{0} minutos'],
 	hours: ['{0} horas', '{0} horas'],
 	title: 'Contador',
-	activeTimer: 'HÃ¡ um contador funcionando.',
+	activeTimer: 'Há um contador funcionando.',
 	actionMerge: 'Adicionar com a atual',
 	remove: 'Parar e remover',
-	openForm: 'Abrir formulÃ¡rio',
+	openForm: 'Abrir formulário',
 	removeConfirm: 'Você realmente deseja excluir?'
 };
